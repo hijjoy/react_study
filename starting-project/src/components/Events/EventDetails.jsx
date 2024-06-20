f4d1b18d269c78f6d74d4b2eea4d9fe0b31b5e27
hijjoy <hijjoy@naver.com>
2024-06-21 01:28:23 +0900

    feat: tanstack query data delete

--- starting-project/src/components/Events/EventDetails.jsx
+++ starting-project/src/components/Events/EventDetails.jsx
@@ -1,8 +1,30 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useNavigate, useParams } from "react-router-dom";
 
-import Header from '../Header.jsx';
+import Header from "../Header.jsx";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { deleteEvent, fetchEvent, queryClient } from "../../util/http.js";
 
 export default function EventDetails() {
+  const path = useParams();
+  const nav = useNavigate();
+
+  const { data } = useQuery({
+    queryKey: ["events", { id: path.id }],
+    queryFn: ({ signal }) => fetchEvent({ signal, id: path.id }),
+  });
+
+  const { mutate } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["events"] });
+      nav("/events");
+    },
+  });
+
+  const handleDelete = () => {
+    mutate({ id: path.id });
+  };
+
   return (
     <>
       <Outlet />
@@ -13,22 +35,26 @@ export default function EventDetails() {
       </Header>
       <article id="event-details">
         <header>
-          <h1>EVENT TITLE</h1>
+          <h1>{data ? data.title : "title"}</h1>
           <nav>
-            <button>Delete</button>
+            <button onClick={handleDelete}>Delete</button>
             <Link to="edit">Edit</Link>
           </nav>
         </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
+        {data && (
+          <div id="event-details-content">
+            <img src={`http://localhost:3000/${data.image}`} alt="image" />
+            <div id="event-details-info">
+              <div>
+                <p id="event-details-location">{data.location}</p>
+                <time dateTime={`Todo-DateT$Todo-Time`}>
+                  {data.date} @ {data.time}
+                </time>
+              </div>
+              <p id="event-details-description">{data.description}</p>
             </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
           </div>
-        </div>
+        )}
       </article>
     </>
   );
